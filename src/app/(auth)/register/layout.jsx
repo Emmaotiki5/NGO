098664b6a31a0b79e16b8592af962a730e9ec3db
Emmaotiki5 <otikie507@gmail.com>
2024-06-14 06:46:45 +0100

    Editing Register

--- src/app/(auth)/register/layout.jsx
+++ src/app/(auth)/register/layout.jsx
@@ -2,16 +2,17 @@ import Side from "./components/side"
 export default function RegisterLayout ({children}) {
     return (
         <>
-            <div className="bg-black text-white" style={{}}>
+            <div className="bg-primary text-white" style={{}}>
                 <div className="mx-auto text-center display-1 mb-5 ">
                     Register
                 </div>
                 <div className="mx-auto">
                     <div className="grid text-start " style={{}}>
-                        <div className="g-col-lg-4 g-col-sm-6 g-start-lg-3 g-start-sm-4 g-col-md-6 g-start-md-4 g-col-12 ">
+                        <div className="g-col-lg-4 g-col-sm-6 g-start-lg-3 g-start-sm-4 g-col-md-12 g-start-md-1 g-col-12 g-start-1 ">
                             <Side />
+                            
                         </div>
-                        <div className="g-col-sm-6 g-col-md-6 g-col-lg-3 g-start-sm-4 g-start-md-4 g-col-12" >
+                        <div className="g-col-sm-6 g-col-md-12 g-col-lg-3 g-start-sm-4 g-start-1 g-start-md-1 g-col-12" >
                             {children}
                         </div>
                     </div>
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -4,51 +4,53 @@ export default function RegisterPage () {
             <div>
                 <div className="card">
                         <div className="card-body ">
-                            <div className="card-header text-center bg-white mb-5">
+                            <div className="card-header text-center bg-light-subtle mb-5">
                                 Create Your Account
                             </div>
-                            <div className="grid mx-auto container">
-                                <div className="">
+                            <div className="grid mx-auto container mb-5 mt-5">
+                                <div className="g-col-4 g-start-lg-3 g-start-3 g-start-md-3 g-start-sm-3">
                                     <button type="button" className="btn btn-outline-dark">
                                         Google
                                     </button>
                                 </div>
-                                <div>
+                                <div className="g-col-4">
                                     <button type="button" className="btn btn-outline-dark">
                                         Facebook
                                     </button>
                                 </div>
                             </div>
-                            <div className="d-flex text-center">
-                                <div>
-                                    <hr />
-                                </div>
-                                Or
-                                <div>
-                                    <hr />
+                            <hr />
+                            <div className="grid mb-5">
+                                <div className="g-col-4 g-start-6 ">
+                                    Or
                                 </div>
                             </div>
+                            <hr />
                             <form action="#" method="post" className="form">
-                                <div>
-                                    <p className="form-label">Username</p>
-                                    <input type="text" className="form-control"/>
+                                <div className="grid mx-auto">
+                                    <div className="g-col-5 g-start-2">
+                                        <p className="form-label">Username</p>
+                                        <input type="text" className="form-control" placeholder="Username"/>
+                                    </div>
+                                    <div className="g-col-5 text-center">
+                                        <p className="form-label">Email</p>
+                                        <input type="email" className="form-control" placeholder='Email'/>
+                                    </div>
                                 </div>
-                                <div>
-                                    <p className="form-label">Email</p>
-                                    <input type="email" className="form-control"/>
-                                </div>
-                                <div>
-                                    <p className="form-label">Password</p>
-                                    <input type="password" className="form-control"/>
-                                </div>
-                                <div>
-                                    <p className="form-label">Password</p>
-                                    <input type="password" className="form-control" autoComplete="false"/>
-                                </div>
-                                <div className="mt-2">
-                                    <button type="submit" className="btn btn-outline-primary ">
-                                        Submit
-                                    </button>
+                                <div className="grid">
+                                    <div className="g-col-6 g-start-4">
+                                        <p className="form-label">Password</p>
+                                        <input type="password" className="form-control"/>
+                                    </div>
+                                    <div className="g-col-6 g-start-4">
+                                        <p className="form-label">Password</p>
+                                        <input type="password" className="form-control" autoComplete="false"/>
+                                    </div>
+                                    <div className="mt-2 g-col-5 g-start-6">
+                                        <button type="submit" className="btn btn-outline-primary ">
+                                            Submit
+                                        </button>
+                                    </div>
                                 </div>
                                 <br />
                             </form>
